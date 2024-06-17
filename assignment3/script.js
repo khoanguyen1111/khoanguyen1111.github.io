974384dd0b2240106f05cd73864e23185f3be4d0
khoanguyen1111 <131126807+khoanguyen1111@users.noreply.github.com>
2024-06-17 20:57:55 +0700

    update

---
+++ assignment3/script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let next = document.querySelector("#next");
+    let prev = document.querySelector("#prev");
+
+    next.addEventListener("click", function () {
+        let items = document.querySelectorAll(".item");
+        document.querySelector(".slide").appendChild(items[0]);
+    });
+
+    prev.addEventListener("click", function () {
+        let items = document.querySelectorAll(".item");
+        document.querySelector(".slide").insertBefore(items[items.length - 1], items[0]);
+    });
+});
